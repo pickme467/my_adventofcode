711dba8a939ee48cd3656aafe8691cb0ff529b01
pickme467 <pawel2267@gmail.com>
2017-12-05 15:20:58 +0100

    Day 3 part two ready

--- elixir/2017/santa/lib/santa.ex
+++ elixir/2017/santa/lib/santa.ex
@@ -162,5 +162,112 @@ defmodule Santa.Day3 do
     round((side_size - 1)/2 +
       abs(nearest_corner_distance - (side_size - 1)/2))
   end
+  @doc """
+  Acceptance test
+  iex> Santa.Day3.part_two()
+  349975
+  """
+  def part_two() do
+    crawl(347991)
+  end
 
+  @doc """
+  iex> Santa.Day3.crawl(24)
+  25
+
+  iex> Santa.Day3.crawl(55)
+  57
+
+  iex> Santa.Day3.crawl(352)
+  362
+  """
+  def crawl(expected_sum) do
+    crawl(7, 7, 6, [3, 2], %{1 => 1, 2 => 1, 3 => 2, 4 => 4, 5 => 5, 6 => 10}, expected_sum)
+  end
+
+  def crawl(index, sum_squares, last_4_squares, next_squares, dictionary, expected_sum) do
+    new_sum = case type(index, sum_squares, next_squares) do
+                :at -> count_at(index, last_4_squares, dictionary)
+                :before -> count_before(index, last_4_squares, dictionary)
+                :after -> count_after(index, last_4_squares, dictionary)
+                :rest -> count_rest(index, last_4_squares, dictionary)
+              end
+    case new_sum >= expected_sum do
+      true -> new_sum
+      false ->
+        new_dictionary = put(index, new_sum, dictionary)
+        {new_index, new_sum_squares,
+         new_last_4, new_next} =
+          count_new_squares(index, sum_squares, last_4_squares, next_squares)
+        crawl(new_index, new_sum_squares, new_last_4, new_next, new_dictionary, expected_sum)
+    end
+  end
+
+  def count_new_squares(index, sum, last_4, next)  do
+    case type(index, sum, next) do
+      :at -> {index + 1, get_new_sum(sum, next), last_4 + 2, get_new_next(next)}
+      _   -> {index + 1, sum, last_4, next}
+    end
+  end
+
+  def get_new_sum(sum, [a, _]) do
+    sum + a
+  end
+
+  def get_new_next([a, a]) do
+    [a + 1, a]
+  end
+
+  def get_new_next([a, _]) do
+    [a, a]
+  end
+
+  def type(index, index, _next) do
+    :at
+  end
+
+  def type(index, sum_squares, [_, _]) when index + 1 == sum_squares do
+                                                    :before
+  end
+
+  def type(index, sum_squares, [_, b]) when index + b - 1 == sum_squares do
+                                                                   :after
+  end
+
+  def type(_, _, _) do
+    :rest
+  end
+
+  def count_at(index, last_4_squares, dictionary) do
+    get_at(index - 1, dictionary) +
+    get_at(index - last_4_squares, dictionary)
+  end
+
+  def count_after(index, last_4_squares, dictionary) do
+    get_at(index - 1, dictionary) +
+    get_at(index - 2, dictionary) +
+    get_at(index - last_4_squares + 1, dictionary) +
+    get_at(index - last_4_squares + 2, dictionary)
+  end
+
+  def count_before(index, last_4_squares, dictionary) do
+    get_at(index - 1, dictionary) +
+    get_at(index - last_4_squares, dictionary) +
+    get_at(index - last_4_squares + 1, dictionary)
+  end
+
+  def count_rest(index, last_4_squares, dictionary) do
+    get_at(index - 1, dictionary) +
+    get_at(index - last_4_squares, dictionary) +
+    get_at(index - last_4_squares + 1, dictionary) +
+    get_at(index - last_4_squares + 2, dictionary)
+  end
+
+  def get_at(index, dictionary) do
+    Map.get(dictionary, index)
+  end
+
+  def put(index, value, dictionary) do
+    Map.put(dictionary, index, value)
+  end
 end
