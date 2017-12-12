a53b1e69b3ce467412dee67361fcb8bea7d030c3
pickme467 <pawel2267@gmail.com>
2017-12-12 20:54:02 +0100

    Day 12 of 2017 complete.

--- elixir/2017/santa/lib/santa.ex
+++ elixir/2017/santa/lib/santa.ex
@@ -958,7 +958,7 @@ defmodule Santa.Day12 do
   """
   def part_one() do
     create_map()
-    |> find_all_connected_to(MapSet.new(["0"]), 1)
+    |> find_all_connected(MapSet.new(["0"]), 1)
     |> MapSet.size()
   end
 
@@ -981,7 +981,7 @@ defmodule Santa.Day12 do
 
   defp find_and_remove(map, sets) do
     element = hd(Map.keys(map))
-    another_set = find_all_connected_to(map, MapSet.new([element]), 1)
+    another_set = find_all_connected(map, MapSet.new([element]), 1)
     new_map = Enum.reduce(MapSet.to_list(another_set), map,
       fn(element, current_map) -> Map.delete(current_map, element) end)
     case length(Map.keys(new_map)) > 0 do
@@ -990,18 +990,16 @@ defmodule Santa.Day12 do
     end
   end
 
-  defp find_all_connected_to(map, all_connected, set_size) do
+  defp find_all_connected(map, all_connected, set_size) do
     new_set = MapSet.new(
       Enum.reduce(MapSet.to_list(all_connected), [],
         fn (key, output) ->
-          case Map.get(map, key, :not_found) do
-            :not_found -> output
-            values -> values ++ output end end))
+          Map.get(map, key) ++ output end))
     new_all_connected = MapSet.union(all_connected, new_set)
     new_size = MapSet.size(new_all_connected)
     case  new_size > set_size do
       true ->
-        find_all_connected_to(map, new_all_connected, new_size)
+        find_all_connected(map, new_all_connected, new_size)
       false -> new_all_connected
     end
   end
