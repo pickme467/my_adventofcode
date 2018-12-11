69b4409b798ecf6540d1299c2a40ccb84924dcd9
pickme467 <pawel2267@gmail.com>
2018-12-11 16:03:54 +0100

    Day 4 of 2018 Advent of Code complete

---
+++ elixir/2018/day4/lib/day4/application.ex
@@ -0,0 +1,20 @@
+defmodule Day4.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    # List all child processes to be supervised
+    children = [
+      # Starts a worker by calling: Day4.Worker.start_link(arg)
+      # {Day4.Worker, arg},
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Day4.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
