4e498dc1b5aa9e6eb7cc18a13cff763ac7d87e36
pickme467 <pawel2267@gmail.com>
2019-12-08 22:44:48 +0100

    AoC 2019, day 7 complete.

--- lisp/2019/day7.lisp
+++ lisp/2019/day7.lisp
@@ -27,6 +27,13 @@
 (defun sample3 () (list 3 31 3 32 1002 32 10 32 1001 31 -2 31 1007 31 0 33
                         1002 33 7 33 1 33 31 31 1 32 31 31 4 31 99 0 0 0))
 
+(defun sample4 () (list 3 26 1001 26 -4 26 3 27 1002 27 2 27 1 27 26
+                        27 4 27 1001 28 -1 28 1005 28 6 99 0 0 5))
+
+(defun sample5 () (list 3 52 1001 52 -5 52 3 53 1 52 56 54 1007 54 5 55 1005 55 26 1001 54
+                        -5 54 1105 1 12 1 53 54 53 1008 54 0 55 1001 55 1 55 2 53 55 53 4
+                        53 1001 56 -1 56 1005 56 6 99 0 0 0 0 10))
+
 (defun get-value (pos-or-value mode list)
   (if (= 0 mode)
       (nth pos-or-value list)
@@ -140,7 +147,7 @@
     (apply #'max  output)))
 
 (defun find-max-amplification-boosted (input-fun)
-    (let ((output ())
+  (let ((output ())
         (vector-output ()))
     (dotimes (m1 5)
       (dotimes (m2 5)
@@ -149,34 +156,25 @@
             (dotimes (m5 5)
               (if (= 5 (length (delete-duplicates (list m1 m2 m3 m4 m5))))
                   (let ((result
-                         (amplifier-values input-fun (+ 5  m1) (+ 5 m2)
-                                           (+ 5 m3) (+ 5 m4) (+ 5 m5))))
+                         (feedback-loop (funcall input-fun) (+ 5  m1) (+ 5 m2)
+                                        (+ 5 m3) (+ 5 m4) (+ 5 m5))))
                     (push (vector result (+ 5  m1) (+ 5 m2)
                                   (+ 5 m3) (+ 5 m4) (+ 5 m5)) vector-output)
                     (push result output))))))))
     (apply #'max  output)))
 
-;; verifications
-(time (assert (= 43210 (amplifier-values #'sample1 4 3 2 1 0))))
-(time (assert (= 54321 (amplifier-values #'sample2 0 1 2 3 4))))
-(time (assert (= 65210 (amplifier-values #'sample3 1 0 4 3 2))))
-
-;; day 7 part 1
-(time (assert (= 117312 (find-max-amplification #'input))))
-
-;; part 2
 (defun calculate-with-interrupt (context)
   (let ((new-context (process-command context)))
     (if (equal 'continue (elt new-context 0))
         (calculate-with-interrupt new-context)
         new-context)))
 
-(defun feedback-loop (p1 p2 p3 p4 p5)
-  (let ((c1 (vector 'continue 0 (input) 0 0))
-        (c2 (vector 'continue 0 (input) 0 0))
-        (c3 (vector 'continue 0 (input) 0 0))
-        (c4 (vector 'continue 0 (input) 0 0))
-        (c5 (vector 'continue 0 (input) 0 0)))
+(defun feedback-loop (input p1 p2 p3 p4 p5)
+  (let ((c1 (vector 'continue 0 (copy-list input) 0 0))
+        (c2 (vector 'continue 0 (copy-list input) 0 0))
+        (c3 (vector 'continue 0 (copy-list input) 0 0))
+        (c4 (vector 'continue 0 (copy-list input) 0 0))
+        (c5 (vector 'continue 0 (copy-list input) 0 0)))
     (setf c1 (calculate-with-interrupt c1))
     (setf (elt c1 4) p1)
     (setf c1 (calculate-with-interrupt c1))
@@ -194,8 +192,40 @@
     (setf c5 (calculate-with-interrupt c5))
     (setf (elt c1 4) 0)
     (setf c1 (calculate-with-interrupt c1))
-    (vector c1 c2 c3 c4 c5)
-    ))
+    (iterate-while-output c1 c2 c3 c4 c5)))
+
+(defun iterate-while-output (c1 c2 c3 c4 c5)
+  (if (equal 'done (elt c5 0))
+      (progn
+        (elt c5 3))
+      (progn
+        (if (equal 'output (elt c1 0))
+            (let ((output (elt c1 3)))
+              (setf c1 (calculate-with-interrupt c1))
+              (setf (elt c2 4) output)
+              (setf c2 (calculate-with-interrupt c2))))
+        (if (equal 'output (elt c2 0))
+            (let ((output (elt c2 3)))
+              (setf c2 (calculate-with-interrupt c2))
+              (setf (elt c3 4) output)
+              (setf c3 (calculate-with-interrupt c3))))
+        (if (equal 'output (elt c3 0))
+            (let ((output (elt c3 3)))
+              (setf c3 (calculate-with-interrupt c3))
+              (setf (elt c4 4) output)
+              (setf c4 (calculate-with-interrupt c4))))
+        (if (equal 'output (elt c4 0))
+            (let ((output (elt c4 3)))
+              (setf c4 (calculate-with-interrupt c4))
+              (setf (elt c5 4) output)
+              (setf c5 (calculate-with-interrupt c5))))
+        (if (equal 'output (elt c5 0))
+            (let ((output (elt c5 3)))
+              (setf c5 (calculate-with-interrupt c5))
+              (setf (elt c1 4) output)
+              (setf c1 (calculate-with-interrupt c1))))
+        (iterate-while-output c1 c2 c3 c4 c5))))
+
 (defun process-command (context)
   (let* ((disposition (elt context 0))
          (start (elt context 1))
@@ -211,7 +241,7 @@
       (declare (ignore p3))
       (cond
         ((= opcode 99)
-         (vector 'done output))
+         (vector 'done start list output 0))
         ((= opcode 1)
          (do-add p1 p2 start list)
          (vector 'continue (+ 4 start) list output 0))
@@ -233,3 +263,18 @@
          (do-equals p1 p2 start list)
          (vector 'continue (+ 4 start) list output 0))
         (t (vector 'error output)) ))))
+
+;; part 1 verifications
+(time (assert (= 43210 (amplifier-values #'sample1 4 3 2 1 0))))
+(time (assert (= 54321 (amplifier-values #'sample2 0 1 2 3 4))))
+(time (assert (= 65210 (amplifier-values #'sample3 1 0 4 3 2))))
+
+;; day 7 part 1
+(time (assert (= 117312 (find-max-amplification #'input))))
+
+;; part 2 verifications
+(time (assert (= 139629729 (feedback-loop (sample4) 9 8 7 6 5))))
+(time (assert (= 18216 (feedback-loop (sample5) 9 7 8 5 6))))
+
+;; day 7 part 2
+(time (assert (= 1336480 (find-max-amplification-boosted #'input))))
