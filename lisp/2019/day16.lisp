7f560a52cd8b113847e5c0958c517b2ffd5eaa58
pickme467 <pawel2267@gmail.com>
2019-12-16 14:07:01 +0100

    AoC 2019, day 16 part one complete.

--- lisp/2019/day16.lisp
+++ lisp/2019/day16.lisp
@@ -5,8 +5,56 @@
 (defun char-to-number (char)
   (- (char-int char) (char-int #\0)))
 
+(defun number-to-char (number)
+  (code-char (+ (char-int #\0) number)))
+
 (defun string-to-number-list (string)
   (map 'list #'char-to-number string))
 
+(defun newhash ()
+  (make-hash-table :test #'equal))
+
+(defun sethash (key value hash)
+  (setf (gethash key hash) value))
+
+(defun list-to-hash (list)
+  (let ((hash (newhash)))
+    (dotimes (n (length list))
+      (sethash n (nth n list) hash))
+    hash))
+
+;; iteration 1-based index 0-based
 (defun get-pattern-for-iteration-index (iteration index)
-  (let ((pattern-size (* 4 iteration)))))
+ (let* ((simple-pattern '(0 1 0 -1))
+        (4-index (mod (floor (1+ index) iteration) 4)))
+   (nth (mod 4-index 4) simple-pattern)))
+
+(defun iterate (hash iteration)
+  (let ((sum 0))
+    (dotimes (n (hash-table-count hash))
+      (let ((element (gethash n hash))
+            (multiplier (get-pattern-for-iteration-index iteration n)))
+        (setf sum (+ sum (* element multiplier)))))
+    (abs (rem sum 10))))
+
+(defun go-n-steps (steps signal)
+  (let ((output (list-to-hash (string-to-number-list signal))))
+      (dotimes (k steps)
+        (let ((hash output))
+          (setf output (newhash))
+          (dotimes (n (hash-table-count hash))
+            (sethash n (iterate hash (1+ n)) output))))
+      output))
+
+(defun get-8-digit-answer (steps input)
+ (let ((answer-hash (go-n-steps steps input)))
+    (map 'string #'number-to-char (get-n 8 answer-hash))))
+
+(defun get-n (times hash)
+  (let ((list ()))
+    (dotimes (n times)
+      (push (gethash n hash) list))
+    (reverse list)))
+
+;; day 16 part 1
+(time (assert (string= "40580215" (get-8-digit-answer 100 (input)))))
