f4606a34c31f56f9923c10e41cb739585f787afc
pickme467 <pawel2267@gmail.com>
2019-12-18 11:40:25 +0100

    AoC 2019, day 16 complete.

--- lisp/2019/day16.lisp
+++ lisp/2019/day16.lisp
@@ -16,18 +16,11 @@
 (defun sethash (key value hash)
   (setf (gethash key hash) value))
 
-(defun geth (n hash)
-  (let ((x (gethash n hash)))
-    (if (null x) 0 x)))
-
-(defun seth (k v hash)
-  (if (= 0 v) nil (sethash k v hash)))
-
 (defun string-to-hash (string)
   (let ((hash (newhash)))
     (dotimes (n (length string))
       (let ((number (char-to-number (elt string n))))
-        (if (/= 0 number) (seth n number hash))))
+        (if (/= 0 number) (sethash n number hash))))
     hash))
 
 ;; iteration 1-based index 0-based
@@ -36,35 +29,65 @@
         (4-index (mod (floor (1+ index) iteration) 4)))
    (nth (mod 4-index 4) simple-pattern)))
 
-(defun iterate (number hash iteration)
+(defun iterate (hash iteration)
   (let ((sum 0))
-    (dotimes (n number)
-      (let ((multiplier (get-pattern-for-iteration-index iteration n)))
-        (if (/= 0 multiplier)
-            (let ((element (geth n hash)))
-              (setf sum (+ sum (* element multiplier)))))))
+    (maphash #'(lambda (index value)
+                 (let ((multiplier (get-pattern-for-iteration-index iteration index)))
+                   (setf sum (+ sum (* value multiplier))))) hash)
     (abs (rem sum 10))))
 
-(defun go-n-steps (steps signal size)
-  (let ((output (string-to-hash signal)))
+(defun go-n-steps (steps signal)
+  (let ((output (string-to-hash signal))
+        (size (length signal)))
       (dotimes (k steps)
         (let ((hash output))
           (setf output (newhash))
           (dotimes (n size)
-            (seth n (iterate size hash (1+ n)) output))))
+            (sethash n (iterate hash (1+ n)) output))))
       output))
 
-(defun get-8-digit-answer (steps input size)
- (let ((answer-hash (go-n-steps steps input size)))
+(defun get-8-digit-answer (steps input)
+ (let ((answer-hash (go-n-steps steps input)))
     (map 'string #'number-to-char (get-n 8 answer-hash))))
 
 (defun get-n (times hash)
   (let ((list ()))
     (dotimes (n times)
-      (push (geth n hash) list))
+      (push (gethash n hash) list))
     (reverse list)))
 
-;; day 16 part 1
-(time (assert (string= "40580215" (get-8-digit-answer 100 (input) (length (input))))))
+(defun pyramid-sum (list)
+  (let* ((sum (reduce #'+ list))
+         (output (list (mod sum 10))))
+    (dotimes (n (1- (length list)))
+      (setf sum (- sum (car list)))
+      (setf output (push (mod sum 10) output))
+      (setf list (cdr list)))
+    output))
 
-(time (assert (string= "12" (get-8-digit-answer 100 (input) (* 10000 (length (input)))))))
+(defun get-offset (input)
+  (parse-integer (subseq input 0 7)))
+
+(defun make-offset-list (input multiplier)
+  (let* ((offset (get-offset input))
+         (original-size (length input))
+         (multiplied-size (* original-size multiplier))
+         (final-size (- multiplied-size offset))
+         (remainder (mod final-size original-size))
+         (full-sets (/ (- final-size remainder) original-size))
+         (output (subseq input (- original-size remainder))))
+    (dotimes (n full-sets)
+      (setf output (concatenate 'string output input)))
+    (string-to-number-list output)))
+
+(defun get-n-digit-answer-part-2 (n input)
+  (let ((list (make-offset-list input 10000)))
+    (dotimes (k 100)
+      (setf list (reverse (pyramid-sum list))))
+    (map 'string #'number-to-char (subseq list 0 n))))
+
+;; day 16 part 1
+(time (assert (string= "40580215" (get-8-digit-answer 100 (input)))))
+
+;; day 16 part 2
+(time (assert (string= "22621597" (get-n-digit-answer-part-2 8 (input)))))
