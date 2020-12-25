c20687c2ba5bf8275c9cd091157af5210a0c8c6b
pickme467 <pawel2267@gmail.com>
2020-12-25 18:48:41 +0100

    AoC 2020 day 23

---
+++ lisp/2020/day2301.lisp
@@ -0,0 +1,60 @@
+(defun day-23-2020-1 ()
+  (rest (iterate-it (input) 100)))
+
+(defun day-23-2020-2 ()
+  (let ((answer (iterate-it (part-2-input (input)) 10000000)))
+    (* (second answer) (third answer))))
+
+(defun iterate-it (input repeat)
+  (let* ((max (length input))
+         (hash (make-hash input))
+         (answer (loop repeat repeat
+                       for (i l) = (single-move-destructive input (last input) max hash)
+                         then (single-move-destructive i l max hash)
+                       finally (return (normalize i)))))
+    answer))
+
+(defun make-hash (input)
+  (let ((hash (make-hash-table :test #'equal)))
+    (loop for i on input
+          do (setf (gethash (first i) hash) i)
+          finally (return hash))))
+
+(defun normalize (list)
+  (loop for i in list
+        for one-found = (= 1 i) then (or one-found (= 1 i))
+        when one-found
+          collect i into first
+        else
+          collect i into second
+        finally (return (append first second))))
+
+(defun single-move-destructive (first last max hash)
+  (declare (optimize (speed 3) (safety 0)))
+  (let* ((three-start (cdr first))
+         (three-end (cdr (cdr three-start)))
+         (first-con first)
+         (after-three (cdr three-end)))
+    (setf (cdr first-con) nil)
+    (setf (cdr last) first-con)
+    (setf (cdr three-end) nil)
+    (let ((to-put-three (gethash (make-candidate (car first-con) max three-start) hash)))
+      (setf (cdr three-end) (cdr to-put-three))
+      (setf (cdr to-put-three) three-start))
+    (list after-three first-con)))
+
+(defun make-candidate (current max removed)
+  (first (loop repeat 4 for i  = (1- current) then (1- i)
+               when (and (not (member i removed)) (> i 0)) collect i
+                 when (and (not (member (+ max i) removed)) (<= i 0)) collect (+ max i))))
+
+(defun part-2-input (input) (part-2-input-max input 1000000))
+
+(defun part-2-input-max (input max)
+  (append input (loop repeat (- max (length input))
+                      for i = (1+ (reduce #'max input)) then (1+ i)
+                      collecting i)))
+
+(defun input () (list 3 6 8 1 9 5 7 4 2))
+
+(defun example () (list 3 8 9 1 2 5 4 6 7))
